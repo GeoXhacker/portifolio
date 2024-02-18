5c06eefe22d73246d65058461ffdde5c0b412b01
GeoXhacker <mukisageophrey@gmail.com>
2024-02-18 07:34:00 +0300

    added nav links

---
+++ src/app/data/links.ts
@@ -0,0 +1,27 @@
+export const NavLinks = [
+  // {
+  //   name: "UX/UI design",
+  //   href: "/"
+  // },
+  // {
+  //   name: "Graphic design",
+  //   href: "/graphic-design"
+  // },
+  // {
+  //   name: "Figma",
+  //   href: "#",
+  //   target: "_blank"
+  // },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/mukisa-geofrey/",
+    target: "_blank"
+  },
+  // {
+  //   name: "Dribble",
+  //   href: "#",
+  //   target: "_blank"
+  // },
+];
+
+export const linkToCV = "#";
